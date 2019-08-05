5e05c21b43b2efd1d82730e4501f8db4be213dbe
AB055474 <AB055474zhangdongbin@ab-insurance.com>
2019-08-05 17:21:01 +0800

    Initial commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.swing.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
